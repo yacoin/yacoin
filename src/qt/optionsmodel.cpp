d310a5b04160680773cede6547f0ab89aece3558
Sairon Istyar <saironiq@gmail.com>
2013-09-23 22:58:19 +0200

    Add Qt GUI CPU PoW mining checkbox

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -169,6 +169,8 @@ QVariant OptionsModel::data(const QModelIndex & index, int role) const
             return QVariant(bDisplayAddresses);
         case DetachDatabases:
             return QVariant(bitdb.GetDetach());
+        case CPUMining:
+            return QVariant(GetBoolArg("-gen"));
         case Language:
             return settings.value("language", "");
         case CoinControlFeatures:
@@ -257,6 +259,12 @@ bool OptionsModel::setData(const QModelIndex & index, const QVariant & value, in
             bool fDetachDB = value.toBool();
             bitdb.SetDetach(fDetachDB);
             settings.setValue("detachDB", fDetachDB);
+            }
+            break;
+        case CPUMining: {
+            bool fCPUMining = value.toBool();
+            GenerateBitcoins(fCPUMining, pwalletMain);
+            mapArgs["-gen"] = (fCPUMining ? "1" : "0");
             }
             break;
         case Language:
