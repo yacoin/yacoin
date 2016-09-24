e8c044717291ce951e155fa15b79748490a3bbe5
yacoin <yacoin@users.noreply.github.com>
2016-09-24 11:41:23 -0400

    Update bitcoin.cpp
    remove "stays on top" per user suggestion

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -210,8 +210,8 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-//    QSplashScreen splash(QPixmap(":/images/splash"), 0);
-    QSplashScreen splash(QPixmap(":/images/splash"), Qt::WindowStaysOnTopHint );	
+    QSplashScreen splash(QPixmap(":/images/splash"), 0);
+//    QSplashScreen splash(QPixmap(":/images/splash"), Qt::WindowStaysOnTopHint );	
     if (GetBoolArg("-splash", true) && !GetBoolArg("-min"))
     {
         splash.show();
