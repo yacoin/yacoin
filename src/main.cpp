b4d58b568460fd5925e0178eccc3f55571e0813e
hbquan93 <hbquan93@gmail.com>
2020-07-05 23:51:57 +0700

    Remove simulation supply of genesis block

--- src/main.cpp
+++ src/main.cpp
@@ -3299,11 +3299,6 @@ bool CBlock::AddToBlockIndex(unsigned int nFile, unsigned int nBlockPos)
         setStakeSeen.insert(make_pair(pindexNew->prevoutStake, pindexNew->nStakeTime));
     pindexNew->phashBlock = &((*mi).first);
 
-    if( 0 == pindexNew->nHeight )
-    {
-        printf("TACA ===> Need to consider if we need to remove this line or not");
-        pindexNew->nMoneySupply = pindexNew->nMint = nSimulatedMOneySupplyAtFork;
-    }
     // Write to disk block index
     CTxDB txdb;
     if (!txdb.TxnBegin())
