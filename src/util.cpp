f89be535025ccc3665cb3d002cc415f84d4de981
dev34253 <dev34253@gmail.com>
2021-01-03 10:25:09 -0500

    [feat] generating initial config file with rpcport 7687 and port 7688

--- src/util.cpp
+++ src/util.cpp
@@ -1261,13 +1261,8 @@ static void createConf()
     pConf.open(GetConfigFile().generic_string().c_str());
     pConf << "rpcuser=user\nrpcpassword="
             + randomStrGen(15)
-#if defined(Yac1dot0)
-            + "\nrpcport=7787"
-            + "\nport=7788"
-#else
             + "\nrpcport=7687"
             + "\nport=7688"
-#endif
             + "\n#(0=off, 1=on) daemon - run in the background as a daemon and accept commands"
             + "\ndaemon=0"
             + "\n#(0=off, 1=on) server - accept command line and JSON-RPC commands"
