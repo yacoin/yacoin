bbc2df1ea2bd45da4293b24703701e23151560ad
dev34253 <dev34253@gmail.com>
2021-01-30 07:53:37 -0500

    [feat] setting version to 1.0.0

--- src/version.cpp
+++ src/version.cpp
@@ -12,13 +12,9 @@
  #include "version.h"
 #endif
 
-#define DISPLAY_VERSION_MAJOR       0
-#define DISPLAY_VERSION_MINOR       5
-#define DISPLAY_VERSION_REVISION    7
-                                    // 4 for new versioning to be more aligned with btc standards
-                                    // 5 for #define DEBUG_LOCKORDER
-                                    // 5 for #define DEBUG_LOCKORDER
-                                    // 6     20160 block length
+#define DISPLAY_VERSION_MAJOR       1
+#define DISPLAY_VERSION_MINOR       0
+#define DISPLAY_VERSION_REVISION    0
 #define DISPLAY_VERSION_BUILD       2
 #define DISPLAY_VERSION_TESTING     01
 
@@ -29,34 +25,23 @@ const int
     DISPLAY_VERSION_BUILD_for_Qt    = DISPLAY_VERSION_BUILD   ,
     DISPLAY_VERSION_TESTING_for_Qt  = DISPLAY_VERSION_TESTING ;
 
-//Set to 1 for Testing Version  But it is set to 1?
-//#define DISPLAY_VERSION_TESTING     1
+// Name of client reported in the getinfo 'version' message. 
 
-// Name of client reported in the 'version' message. Report the same name
-// for both yacoind and yacoin-qt, to make it harder for attackers to
-// target servers or GUI users specifically.
-
-// this is a TestNet only version
 const std::string 
     #ifdef QT_GUI
         #ifdef _MSC_VER
-            CLIENT_NAME("Yacoin-WM MSVC++ Qt");
+            CLIENT_NAME("Yacoin-WM Windows Qt");
         #else
             CLIENT_NAME("Yacoin-WM Qt");
         #endif
     #else
         #ifdef _MSC_VER
-            CLIENT_NAME("Yacoin-WM MSVC++ daemon");
+            CLIENT_NAME("Yacoin-WM Windows daemon");
         #else
             CLIENT_NAME("Yacoin-WM daemon");
         #endif
     #endif
-//    #ifdef _MSC_VER
-//        CLIENT_NAME("Yacoin-WM MSVC++");
-//    #else
-//        CLIENT_NAME("Yacoin-WM");
-//    #endif
-//}
+
 // Client version number
 #ifdef USE_LEVELDB
 #ifdef LOW_DIFFICULTY_FOR_DEVELOPMENT
@@ -72,10 +57,6 @@ const std::string
 #ifdef HAVE_BUILD_INFO
 #    include "build.h"
 #endif
-
-//#define BUILD_DESC_INFO(maj,min,rev) 
-//  "YAC-v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev)
-//    DO_STRINGIZE(maj) 
 
 #define BUILD_DESC_INFO(maj,min,rev,build) \
     "YAC-v" \
