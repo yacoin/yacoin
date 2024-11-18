63566388f6b4a921971a9f21e748b8b9c8f88f08
Nguyen Thai Chau <chaunguyen4392@gmail.com>
2024-11-18 07:05:00 +0700

    Remove PoS mining logic

--- src/miner.h
+++ src/miner.h
@@ -30,9 +30,6 @@ void FormatHashBuffers_64bit_nTime(char* pblock, char* pmidstate, char* pdata,
 /** Check mined proof-of-work block */
 bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey);
 
-/** Check mined proof-of-stake block */
-bool CheckStake(CValidationState& state, CBlock* pblock, CWallet& wallet);
-
 /** Base sha256 mining transform */
 void SHA256Transform(void* pstate, void* pinput, const void* pinit);
 
